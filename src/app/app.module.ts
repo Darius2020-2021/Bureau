3a0042a4209c146b2b7c06210b5edcd67ff1209e
Darius2020-2021 <mohamedtine077@gmail.com>
2021-01-19 10:32:53 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarreComponent } from './barre/barre.component';
+import { ListeComponent } from './liste/liste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarreComponent,
+    ListeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/barre/barre.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barre',
+  templateUrl: './barre.component.html',
+  styleUrls: ['./barre.component.scss']
+})
+export class BarreComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
