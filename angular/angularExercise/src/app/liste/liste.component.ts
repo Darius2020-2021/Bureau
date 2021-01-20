173b3fe5c053620851a80d0d25902537ed5a21c5
Darius2020-2021 <mohamedtine077@gmail.com>
2021-01-20 22:25:46 +0000

    nouveau programme

--- angular/angularExercise/src/app/liste/liste.component.ts
+++ angular/angularExercise/src/app/liste/liste.component.ts
@@ -7,56 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeComponent implements OnInit {
 
-  Iphone =[{
+  Iphones =[{
 
-    produit:"iphone1" },
+    produit:"iphone1" ,
+    description : "l'iphone 1 est sorti en 2007 " },
   {
 
-  produit:" iphone2" },
+  produit:" iphone2" ,
+  description:"l'iphone 2 est sorti en 2008 "
+},
   {
 
-  produit:"iphone3" },
-      {
+  produit:"iphone3" ,
+  description:"l'iphone 3 est sorti en 2009"
+},
+{
+produit:" iphone4 " ,
+description:"l'iphone 4 est sorti en 2010"
+},
+{
 
-        produit:" iphone4 " },
-        {
-
-          produit:" iphone5" },
-          {
-
-            produit:"iphone6" },
-            {
-
-              produit:"iphone7 " },
-              {
-
-                produit:"iphone8" },
-              ];    
+produit:" iphone5" ,
+description:"l'iphone 5 est sorti en 2011 "
+},
+{
+produit:"iphone6" ,
+description:"l'iphone 6 est sorti en 2012"
+},
+{
+produit:"iphone7 " ,
+description:"l'iphone 7 est sorti en 2013"
+},
+{
+produit:"iphone8",
+description:"l'iphone 8 est sorti en 20014 "
+},
+];                 
+achat(){
+  window.alert("l'achat a été écffectué");
+  }
+precomander(){
+  window.alert("votre produits a été précommander")
+}
  
-  iphone=[
-    {description:"l'iphone 1 est sorti en 2007 "},
-  {
-    description:"l'iphone 2 est sorti en 2008 "
-  },
-  {
-    description:"l'iphone 3 est sorti en 2009"
-  },
-  {
-    description:"l'iphone 4 est sorti en 2010"
-  },
-  {
-    description:"l'iphone 5 est sorti en 2011 "
-  },
-  {
-    description:"l'iphone 6 est sorti en 2012"
-  },
-  {
-    description:"l'iphone 7 est sorti en 2013"
-  },
-  {
-    description:"l'iphone 8 est sorti en 20014 "
-  },
-  ];
   constructor() { }
 
   ngOnInit(): void {
