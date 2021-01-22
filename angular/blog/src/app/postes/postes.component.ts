e41903c05aab335c73010aed8fa814b413048b4f
Darius2020-2021 <mohamedtine077@gmail.com>
2021-01-22 08:10:16 +0000

    suite

--- angular/blog/src/app/postes/postes.component.ts
+++ angular/blog/src/app/postes/postes.component.ts
@@ -9,17 +9,14 @@ export class PostesComponent implements OnInit {
   postes=[{
     title_poste:"Mon premier poste",
     description:"j'apprend a devenir le meilleur devloppeur et j'y arriverais , je rencontrera des tas d'obstacles mais j'abandonnerais pas",
-    color:'#9CEF5A'
   },
   {
     title_poste:"Mon seconde poste",
     description:"j'apprend a devenir le meilleur devloppeur et j'y arriverais , je rencontrera des tas d'obstacles mais j'abandonnerais pas",
-    color: '#EC7063'
   },
   {
-    title_poste:"Mon troisieme poste",
+    title_poste:"Mon dernier poste",
     description:"j'apprend a devenir le meilleur devloppeur et j'y arriverais , je rencontrera des tas d'obstacles mais j'abandonnerais pas",
-    color:""
   }
 ];
 love(){
