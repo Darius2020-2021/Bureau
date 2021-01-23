b2d4d3ba7a12898896246f368bf3c3278ed8f9e4
Darius2020-2021 <76709772+Darius2020-2021@users.noreply.github.com>
2021-01-23 22:20:40 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  path="profile",
+  component:ProfilComponent];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
