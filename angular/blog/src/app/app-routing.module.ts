6e4fede34e2ef0db55919e3d8226bc2abe72bf2f
Darius2020-2021 <mohamedtine077@gmail.com>
2021-01-26 12:14:46 +0000

    le rout

--- angular/blog/src/app/app-routing.module.ts
+++ angular/blog/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostesComponent } from './postes/postes.component';
 import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
-  { path:'profile', 
-    component: ProfilComponent
-  },
-
+  {path: '', component: PostesComponent},
+  {path:'profile', component: ProfilComponent}
 ];
 
 @NgModule({
